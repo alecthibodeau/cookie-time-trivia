691cf4c21a93507d8ac0458bcb005f53246c3a0e
alec t <alec@alect.me>
2022-06-25 14:55:17 -0400

    Add Sass and create SCSS stylesheet file

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
 import { StrictMode} from 'react';
 import { createRoot } from 'react-dom/client';
 
+/* Components */
 import App from './App';
+
+/* Styles */
+import '../src/styles/styles.scss';
 
 // The correct ID of the root element is the ID of the div in index.html
 const rootElement = document.getElementById('root');
