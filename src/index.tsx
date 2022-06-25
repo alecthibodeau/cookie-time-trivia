00cb47bc23e47a742d0c50dedd147cc2f0c1f32c
alec t <alec@alect.me>
2022-06-25 14:09:51 -0400

    Update App and index TSX files

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { StrictMode} from 'react';
+import { createRoot } from 'react-dom/client';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+// The correct ID of the root element is the ID of the div in index.html
+const rootElement = document.getElementById('root');
+
+// For TypeScript add the non-null (!) assertion operator
+const root = createRoot(rootElement!);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
