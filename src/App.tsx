84414f1340596a1d977c3e731ddd75a03a3e6030
alec t <alec@alect.me>
2022-06-25 19:53:23 -0400

    Import Emotion to write styles with objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+/** @jsxImportSource @emotion/react */
+
+/* Styles */
+import appStyles from './styles/app-styles';
+
 function App() {
   return (
-    <div>
+    <div css={appStyles.mainDiv}>
       <h1>
         Cookie Time Trivia
       </h1>
---
+++ src/styles/app-styles.ts
@@ -0,0 +1,10 @@
+const appStyles = {
+
+  mainDiv: {
+    padding: '4rem',
+    backgroundColor: '#ff0',
+    borderRadius: '1rem'
+  }
+}
+
+export default appStyles;
